cd2c273ca4e1d7fb67dcebe0593acd8dfa034e91
Jasonng2001 <nguyenbathuan2001@gmail.com>
2024-12-11 23:02:29 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+ var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker."],
+typeSpeed:50,
+backSpeed:10,
+loop:true,
+});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+$('.counter').countUp();
